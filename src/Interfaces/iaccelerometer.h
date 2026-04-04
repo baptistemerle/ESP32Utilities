202f9be1b0e3e9312d5b3cf0795f712f42516134
Baptiste Merle <9810925+baptistemerle@users.noreply.github.com>
2026-04-04 18:08:09 +0200

    Initial files

---
+++ src/Interfaces/iaccelerometer.h
@@ -0,0 +1,10 @@
+#ifndef IACCELEROMETER_H
+#define IACCELEROMETER_H
+
+class IAccelerometer
+{
+public:
+  virtual bool getValues(float& x, float& y, float& z) = 0;
+};
+
+#endif // IACCELEROMETER_H
