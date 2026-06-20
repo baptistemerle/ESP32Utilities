38d2df993c70518b6f3ede86bb0d90214a6bf39d
Baptiste Merle <9810925+baptistemerle@users.noreply.github.com>
2026-06-20 15:34:15 +0200

    Upgrade LVGL version, remove LovyanGFX dependency, migrate to ESP-IDF

--- src/Interfaces/idisplay_driver.h
+++ src/Interfaces/idisplay_driver.h
@@ -3,14 +3,30 @@
 
 #include <stdint.h>
 
+enum class DisplayRenderMode
+{
+  Partial,
+  Direct
+};
+
+using DisplayTxDoneCallback = void (*)(void* arg);
+
 class IDisplayDriver
 {
 public:
-  virtual void init() = 0;
-  virtual void flush(int32_t x1, int32_t y1, int32_t x2, int32_t y2, void* rawData) = 0;
+  virtual ~IDisplayDriver() = default;
+
+  virtual void init(DisplayTxDoneCallback callback, void* callbackArg) = 0;
+
+  virtual void flush(int32_t x1, int32_t y1, int32_t x2, int32_t y2, const void* rawData) = 0;
 
   virtual int width() const = 0;
   virtual int height() const = 0;
+  virtual DisplayRenderMode preferredRenderMode() const = 0;
+
+  virtual bool requiresByteSwap() const { return false; }
+
+  virtual void* getFramebuffer(int index) const { return nullptr; }
 };
 
 #endif // IDISPLAY_DRIVER_H
